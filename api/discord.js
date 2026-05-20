663f76d9096903dab770e3807f2b254630c33cb5
Noamantaka <noaman@takadao.io>
2026-05-21 00:47:06 +0300

    use tweetnacl for verification

--- api/discord.js
+++ api/discord.js
@@ -1,6 +1,22 @@
+const nacl = require("tweetnacl");
+
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).send("Method Not Allowed");
+  }
+
+  const signature = req.headers["x-signature-ed25519"];
+  const timestamp = req.headers["x-signature-timestamp"];
+  const body = JSON.stringify(req.body);
+
+  const isVerified = nacl.sign.detached.verify(
+    Buffer.from(timestamp + body),
+    Buffer.from(signature, "hex"),
+    Buffer.from(process.env.DISCORD_PUBLIC_KEY, "hex")
+  );
+
+  if (!isVerified) {
+    return res.status(401).send("Invalid signature");
   }
 
   const { type } = req.body;
