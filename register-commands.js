8be626db1c7e29a83f2cbfde500c7927709315ec
Noamantaka <noaman@takadao.io>
2026-05-21 01:24:09 +0300

    create thread per conversation

--- register-commands.js
+++ register-commands.js
@@ -22,17 +22,21 @@ const commands = [
   },
 ];
 
-const response = await fetch(
-  `https://discord.com/api/v10/applications/${APPLICATION_ID}/commands`,
-  {
-    method: "PUT",
-    headers: {
-      Authorization: `Bot ${DISCORD_BOT_TOKEN}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(commands),
-  }
-);
+async function register() {
+  const response = await fetch(
+    `https://discord.com/api/v10/applications/${APPLICATION_ID}/commands`,
+    {
+      method: "PUT",
+      headers: {
+        Authorization: `Bot ${DISCORD_BOT_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(commands),
+    }
+  );
 
-const data = await response.json();
-console.log("✅ Commands registered:", data);
+  const data = await response.json();
+  console.log("✅ Commands registered:", data);
+}
+
+register();
