7a9c687d33a907b54c063e946be219b5ee3a0d03
Noamantaka <noaman@takadao.io>
2026-05-22 12:02:29 +0300

    remove debug code

--- api/intercom.js
+++ api/intercom.js
@@ -14,13 +14,8 @@ module.exports = async function handler(req, res) {
     eventType === "conversation.user.created" ||
     eventType === "conversation.admin.replied";
 
-    if (!isMessageEvent) {
-    // بعت الـ event type لـ Discord عشان نشوفه
-    await fetch(process.env.DISCORD_WEBHOOK_URL, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ content: `🔍 Ignored event: **${eventType}**` }),
-    });
+  if (!isMessageEvent) {
+    console.log("Ignored event type:", eventType);
     return res.status(200).json({ ignored: true });
   }
 
